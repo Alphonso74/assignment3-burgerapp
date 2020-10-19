89a34833b99e577a18b320d0c0324adfec99c4d1
Alphonso74 <ajm6684@psu.edu>
2020-10-19 17:01:05 -0400

    Alot of additions

---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import Aux from '../../HOC/Aux';
+import Burger from '../../Components/Burger/Burger'
+
+class BurgerBuilder extends Component{
+
+    state = {
+      ingredients: {
+          salad: 0,
+          bacon: 0,
+          cheese: 0,
+          meat: 0
+      }
+
+
+    };
+
+    render() {
+
+        return(
+            <Aux>
+                <div>
+                    <Burger ingredients = {this.state.ingredients}/>
+                </div>
+                <div>
+                    Build Controls
+                </div>
+
+            </Aux>
+        );
+    }
+
+}
+
+export default BurgerBuilder;
